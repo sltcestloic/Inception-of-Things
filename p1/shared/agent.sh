4d11401b1ea19eb927e217a48485cb60a45eeefd
ctaleb <ctaleb@student.4lyon.fr>
2025-01-28 14:13:03 +0100

    fix(k3s): fixed k3s issues by changing box; some attempts at making the provisioning more configurable

--- p1/shared/agent.sh
+++ p1/shared/agent.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-# Get the controller node IP
-MASTER_IP=$1
 
-# Get the token from the shared folder
-TOKEN=$(cat /vagrant/token)
+# Get vars
+SHARED_FOLDER=$1
+MASTER_IP=$2
 
-# Install K3s agent (worker) and join the master node
-# ? INSTALL_K3S_EXEC="--flannel-iface=enp0s8"
-curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
+# Ready the environment for K3s
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install curl
+
+# Install K3s agent
+export K3S_URL=https://$MASTER_IP:6443
+# export K3S_URL=https://192.168.58.110:6443
+export K3S_TOKEN_FILE=/$SHARED_FOLDER/token
+export INSTALL_K3S_EXEC="--flannel-iface=eth1"
+# curl -sfL https://get.k3s.io |  sh -
--- p1/shared/controller.sh
+++ p1/shared/controller.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+
+# Get vars
+SHARED_FOLDER=$1
+MASTER_IP=$2
+
+# Ready the environment for K3s
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install curl
+
 # Install K3s
-# ? INSTALL_K3S_EXEC="--bind-address=192.168.58.110 --node-external-ip=192.168.58.110 --flannel-iface=enp0s8" K3S_KUBECONFIG_MODE="644"
-curl -sfL https://get.k3s.io | sh -
+export K3S_KUBECONFIG_MODE="644" k3s server
+export INSTALL_K3S_EXEC="--bind-address=$2 --node-external-ip=$2 --flannel-iface=eth1"
+# curl -sfL https://get.k3s.io | sh -
 
-# Make sure kubectl is set up for the vagrant user
-sudo mkdir -p /home/vagrant/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
-
-# Get the token for the worker nodes
-TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
-
-# Store the token for the workers to use
-echo $TOKEN > /vagrant/token
+# Copy config and token to shared folder for future use
+# sudo cp /etc/rancher/k3s/k3s.yaml $SHARED_FOLDER/
+# sudo cp /var/lib/rancher/k3s/server/node-token $SHARED_FOLDER/token
