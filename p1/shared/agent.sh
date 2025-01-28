9903495372e2b69260fc3eb1c8c34e759a69d284
ctaleb <ctaleb@student.4lyon.fr>
2025-01-28 15:46:37 +0100

    fix(provisionning): changed to apt-get to be able to upgrade; re-enabled k3s installations

--- p1/shared/agent.sh
+++ p1/shared/agent.sh
@@ -5,13 +5,12 @@ SHARED_FOLDER=$1
 MASTER_IP=$2
 
 # Ready the environment for K3s
-sudo apt update
-# sudo apt upgrade -y
-sudo apt install curl
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install curl -y
 
 # Install K3s agent
 export K3S_URL=https://$MASTER_IP:6443
-# export K3S_URL=https://192.168.58.110:6443
 export K3S_TOKEN_FILE=/$SHARED_FOLDER/token
 export INSTALL_K3S_EXEC="--flannel-iface=eth1"
-# curl -sfL https://get.k3s.io |  sh -
+curl -sfL https://get.k3s.io |  sh -
