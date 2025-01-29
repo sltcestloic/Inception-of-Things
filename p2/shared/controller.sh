3596945ac3f784a09f11968c410bdeada83e9336
sltcestloic <root@sltcestloic.dev>
2025-01-29 10:32:51 +0100

    part 2 ok me thinks

---
+++ p2/shared/controller.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get vars
+MASTER_IP=$1
+
+# Ready the environment for K3s
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install curl -y
+
+# Install K3s
+export K3S_KUBECONFIG_MODE="644" k3s server
+export INSTALL_K3S_EXEC="--bind-address=$MASTER_IP --node-external-ip=$MASTER_IP --flannel-iface=eth1"
+curl -sfL https://get.k3s.io | sh -
+
+kubectl apply -f /vagrant/app1/
+kubectl apply -f /vagrant/app2/
+kubectl apply -f /vagrant/app3/
+kubectl apply -f /vagrant/ingress.yaml
+
+# Wait for all deployments to be available
+kubectl wait --for=condition=available deployment --all --timeout=300s
