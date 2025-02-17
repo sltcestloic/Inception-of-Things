73fa383f551bff5c77e13a3c52fb97c0b22fe2a3
sltcestloic <root@sltcestloic.dev>
2025-02-17 08:51:18 +0100

    add install docker to startup script

--- startup.sh
+++ startup.sh
@@ -20,6 +20,17 @@ curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.t
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
+# Install Docker
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 # Install K3D
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
